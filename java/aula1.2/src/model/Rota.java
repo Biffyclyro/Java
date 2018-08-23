6aea75ae4911c7335594e65e790b9f6404bd5cd2
Biffyclyro <joao-senna1@hotmail.com>
2018-08-23 10:01:51 -0300

    Update Rota.java

--- java/aula1.2/src/model/Rota.java
+++ java/aula1.2/src/model/Rota.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author aluno
- */
+import java.util.ArrayList;
 public class Rota {
-    
+    public ArrayList<Clientes> lista = new ArrayList();    
 }
+
