f129725464f749ace4d9f033c081a2460fba2226
Biffyclyro <joao-senna1@hotmail.com>
2018-08-23 15:42:32 -0300

    Update Cliente.java

--- java/aula1.2/src/model/Cliente.java
+++ java/aula1.2/src/model/Cliente.java
@@ -1,21 +1,52 @@
 package model;
 
 public class Cliente {
-    public String nome;
-    public String rua;
-    public int numero;
-    public String cpf;    
     
-    public Boolean comprar(int x){
-        if(x == 1){
-            return true;
-        }else{
-            return false;
-        }
-        
+    private String nome;
+    private String rua;
+    private int numero;
+    private String cpf;
+    
+    /*visibilidade 
+    tipo de retorno
+    nome do método(com ou sem atributos)
+    corpo do método(bloco de código)
+    */
+    public void setNome(String nome){
+        this.nome = nome;        
+    }
+    //this usado para referenciar o valor da variável no objeto
+    public void setRua(String rua){
+        this.rua = rua;
     }
     
-    public String apresentacao(){
-        return "Nome: " + nome + " Rua: " + rua + " Numero: " + numero + " CPF: " + cpf;
+    public void setNumero(int numero){
+        this.numero = numero;
     }
+    
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    
+    // get sem parâmetro
+    public String getNome(){
+        return nome;
+    }
+    
+    public String getRua(){
+        return rua;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public String getCpf(){
+        return cpf;
+    }
+    public Cliente(String cpf){
+        System.out.println("Criando cliente...");
+        setCpf(cpf);
+    }
+    
 }
