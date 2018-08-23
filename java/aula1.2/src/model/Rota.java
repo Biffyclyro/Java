2e9be3716b08a84276002e69854e6458a8a3fc83
Biffyclyro <joao-senna1@hotmail.com>
2018-08-23 16:47:40 -0300

    Update Rota.java

--- java/aula1.2/src/model/Rota.java
+++ java/aula1.2/src/model/Rota.java
@@ -1,7 +1,25 @@
 package model;
 
 import java.util.ArrayList;
-public class Rota {
-    public ArrayList<Clientes> lista = new ArrayList();    
-}
 
+public class Rota {
+    private String nome;
+    private ArrayList<Cliente> pontosEntrega;
+    
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    
+    
+    public void setPontosEntrega(ArrayList<Cliente> clientes){
+        this.pontosEntrega = clientes;
+    }
+    
+    public ArrayList<Cliente> getPontosEntrega(){
+        return pontosEntrega;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+}
